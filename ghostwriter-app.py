557c17a372453a0c9caaf5df3c53cda3913b4c05
yiting-tsai <clara719tsai@gmail.com>
2021-01-20 15:11:24 +0100

    Use cache to load model in app.py

--- ghostwriter-app.py
+++ ghostwriter-app.py
@@ -7,19 +7,24 @@ from transformers import GPT2Tokenizer
 st.set_page_config(page_title='Rap Ghostwriter')
 
 #---------------------------------#
-# Model loading
-model = GPT2LMHeadModel.from_pretrained('model/out').to('cpu') # because its loaded on xla by default
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+# Model loading function to cache
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model = GPT2LMHeadModel.from_pretrained('model/out').to('cpu') # because its loaded on xla by default
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    return model, tokenizer
 
 #---------------------------------#
+
 st.write("""
 # The Rap Ghostwriter App
-The model is built with GPT-2 trained on top 20 popular song lyrics of each rap/hip-hop artist listed on annual Top 50 of BillBoard from last 10 years. Trained on TPUs via Pytorch/XLA for less than 30 mins.
-
+The model is built with GPT-2 trained on top 20 popular song lyrics of each rap/hip-hop artist listed on annual Top 50 of BillBoard from last 10 years. 
+Trained on TPUs via Pytorch/XLA for less than 30 mins.
+    
 :exclamation: Explicit contents: some ***profane words*** and ***racial slurs*** might be present in generated text.
 
 In the following section, please input a word, a phrase or a paragraph as you wish, 
-and also how long would you like the text to be?  
+and also how long would you like the text to be?
 """)
 
 default_value_start_prompt="""I'm tired of being the one
@@ -31,11 +36,12 @@ It's all I see"""
 start_prompt=st.text_area("Start prompt (a word, phrase, paragraph)", default_value_start_prompt)
 max_len=st.text_input("Length for texts to be generated", 250)
 max_len_int=int(max_len)
+model, tokenizer=load_model()
 
 inputs=tokenizer.encode(start_prompt, add_special_tokens=False, return_tensors="pt")
-prompt_length = len(tokenizer.decode(inputs[0], skip_special_tokens=True, clean_up_tokenization_spaces=True))
-outputs = model.generate(inputs, max_length=max_len_int, do_sample=True, top_p=0.95, top_k=60)
-generated = tokenizer.decode(outputs[0])
+prompt_length=len(tokenizer.decode(inputs[0], skip_special_tokens=True, clean_up_tokenization_spaces=True))
+outputs=model.generate(inputs, max_length=max_len_int, do_sample=True, top_p=0.95, top_k=60)
+generated=tokenizer.decode(outputs[0])
 
 st.write(":ghost: ghost might need a couple of minutes to write (hey, it's not easy for them to grab physical objects!) and once you reclick that button beneath, previous generated texts would be gone :dash:")
 if st.button('Write me some texts, Ghost!'):
