6a9bc83622684568f0c0e456af2b12cd8d2388b0
yiting-tsai <clara719tsai@gmail.com>
2021-01-20 14:29:44 +0100

    Minor correction on app.py

--- ghostwriter-app.py
+++ ghostwriter-app.py
@@ -4,9 +4,7 @@ from transformers import GPT2Tokenizer
 
 #---------------------------------#
 # Page layout
-## Page expands to full width
-st.set_page_config(page_title='Rap Ghostwriter',
-    layout='wide')
+st.set_page_config(page_title='Rap Ghostwriter')
 
 #---------------------------------#
 # Model loading
@@ -16,24 +14,28 @@ tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 #---------------------------------#
 st.write("""
 # The Rap Ghostwriter App
-The model is built with GPT-2 trained on top 20 popular song lyrics of each top 50 rap/hip-hop artists listed on BillBoard for last 10 years. Trained on TPUs via Pytorch/XLA for less than 30 mins.
+The model is built with GPT-2 trained on top 20 popular song lyrics of each rap/hip-hop artist listed on annual Top 50 of BillBoard from last 10 years. Trained on TPUs via Pytorch/XLA for less than 30 mins.
 
 In the following section, please input a word, a phrase or a paragraph as you wish, 
 and also how long would you like the text to be?  
 """)
 st.write(":exclamation: Some ***profane words*** and ***racial slurs*** might be present in generated text.")
 
-default_value_start_prompt="I'm too turned up"
+default_value_start_prompt="""I'm tired of being the one
+'Cause I see the sunrise when it comes
+In your face
+A new woman, that's how I feel
+It's all I see"""
 
 start_prompt=st.text_area("Start prompt (a word, phrase, paragraph)", default_value_start_prompt)
-max_len=st.text_input("label goes here", 250)
+max_len=st.text_input("Length for texts to be generated", 250)
 max_len_int=int(max_len)
 
 inputs=tokenizer.encode(start_prompt, add_special_tokens=False, return_tensors="pt")
 prompt_length = len(tokenizer.decode(inputs[0], skip_special_tokens=True, clean_up_tokenization_spaces=True))
 outputs = model.generate(inputs, max_length=max_len_int, do_sample=True, top_p=0.95, top_k=60)
-generated = tokenizer.decode(outputs[0])[prompt_length:]
+generated = tokenizer.decode(outputs[0])
 
+st.write(":ghost: ghost might need a couple of minutes to write, but hey, it's not easy for them to grab physical elements.")
 if st.button('Write me some texts, Ghost!'):
-    st.header('Text generated:')
-    st.write(generated)
+    st.text_area('Text generated:',generated,height=800)
