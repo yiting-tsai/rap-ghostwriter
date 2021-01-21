5fb044c566df27cc97c1525fa6d59a954d5a1821
TSAI Yi-Ting <clara719tsai@gmail.com>
2021-01-21 18:41:31 +0100

    Correction in app.py and add tensorflow package

--- ghostwriter-app.py
+++ ghostwriter-app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from transformers import GPT2LMHeadModel
+from transformers import TFGPT2LMHeadModel
 from transformers import GPT2Tokenizer
 from transformers import GPT2Config
 
@@ -12,7 +13,7 @@ st.set_page_config(page_title='Rap Ghostwriter')
 @st.cache(show_spinner=False)
 def load_model():
     config=GPT2Config.from_json_file('./model/out/config.json')      #local_files_only=True
-    model=GPT2LMHeadModel.from_pretrained('./model/out/pytorch_model.bin', from_pt=True, config=config).to('cpu') # because its loaded on xla by default
+    model=TFGPT2LMHeadModel.from_pretrained('./model/out/pytorch_model.bin', from_pt=True, config=config).to('cpu') # because its loaded on xla by default
     tokenizer=GPT2Tokenizer.from_pretrained('gpt2')
     return model, tokenizer
 
