2ce53a4854bf9c5405a9ab951c1563bbeda11005
yiting-tsai <clara719tsai@gmail.com>
2021-01-21 22:03:23 +0100

    Use gpt_2_simple model weights

--- ghostwriter-app.py
+++ ghostwriter-app.py
@@ -1,18 +1,20 @@
 import streamlit as st
-from transformers import GPT2LMHeadModel
-from transformers import TFGPT2LMHeadModel 
-from transformers import GPT2Tokenizer
-from transformers import GPT2Config
+#from transformers import GPT2LMHeadModel
+#from transformers import TFGPT2LMHeadModel 
+#from transformers import GPT2Tokenizer
+#from transformers import GPT2Config
+from tensorflow.python.compiler.tensorrt import trt_convert as trt
+import gpt_2_simple as gpt2
 
 #---------------------------------#
 # Page layout
 st.set_page_config(page_title='Rap Ghostwriter')
 
 #####
-import os
-st.write(os.getcwd())
-st.write(os.listdir())
-import pathlib
+#import os
+#st.write(os.getcwd())
+#st.write(os.listdir())
+#import pathlib
 # pathlib.Path().absolute()
 #####
 
@@ -20,11 +22,17 @@ import pathlib
 # Model loading function to cache
 @st.cache(show_spinner=False)
 def load_model():
+    sess = gpt2.start_tf_sess()
+    run_name = 'run1'
+    return sess, run_name
+
+
+## HuggingFace gpt-2
     #config=GPT2Config.from_json_file('./model/out/config.json')      # local_files_only=True
     #model=TFGPT2LMHeadModel.from_pretrained('./model/out/pytorch_model.bin', from_pt=True, config=config, local_files_only=True).to('cpu')
-    model=GPT2LMHeadModel.from_pretrained(pretrained_model_name_or_path='./model/out/').to('cpu') # because its loaded on xla by default
-    tokenizer=GPT2Tokenizer.from_pretrained('gpt2')
-    return model, tokenizer
+    #model=GPT2LMHeadModel.from_pretrained(pretrained_model_name_or_path='./model/out/').to('cpu') # because its loaded on xla by default
+    #tokenizer=GPT2Tokenizer.from_pretrained('gpt2')
+    #return model, tokenizer
 
 #---------------------------------#
 
@@ -48,12 +56,31 @@ It's all I see"""
 start_prompt=st.text_area("Start prompt (a word, phrase, paragraph)", default_value_start_prompt)
 max_len=st.text_input("Length for texts to be generated", 250)
 max_len_int=int(max_len)
-model, tokenizer=load_model()
 
-inputs=tokenizer.encode(start_prompt, add_special_tokens=False, return_tensors="pt")
-prompt_length=len(tokenizer.decode(inputs[0], skip_special_tokens=True, clean_up_tokenization_spaces=True))
-outputs=model.generate(inputs, max_length=max_len_int, do_sample=True, top_p=0.95, top_k=60)
-generated=tokenizer.decode(outputs[0])
+# inference
+gpt2.load_gpt2(sess)
+generated=gpt2.generate(sess,\
+    run_name=run_name,\
+    length=max_len_int,\
+    # higher the temperature, the crazier the text 
+    # default 0.7, recommended to keep between 0.7 and 1.0
+    temperature=0.9,\
+    # limits the generated guesses to the top k guesses
+    # if the generated output is super crazy, you may want to set top_k=40
+    top_k=88,\
+    # nucleus sampling: limits the generated guesses to a cumulative probability
+    # good results > 0.9 
+    top_p=0.9,\
+    prefix=start_prompt,\
+    nsamples=1, # set same number as batch_size
+    batch_size=1)
+
+## HuggingFace gpt-2
+#model, tokenizer=load_model()
+#inputs=tokenizer.encode(start_prompt, add_special_tokens=False, return_tensors="pt")
+#prompt_length=len(tokenizer.decode(inputs[0], skip_special_tokens=True, clean_up_tokenization_spaces=True))
+#outputs=model.generate(inputs, max_length=max_len_int, do_sample=True, top_p=0.95, top_k=60)
+#generated=tokenizer.decode(outputs[0])
 
 st.write(":ghost: ghost might need a couple of minutes to write (hey, it's not easy for them to grab physical objects!) and once you reclick that button beneath, previous generated texts would be gone :dash:")
 if st.button('Write me some texts, Ghost!'):
